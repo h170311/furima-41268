49004f30948159882238b3a1704d6c7e636896fe
pmrfx387 <h170311@tg.commufa.jp>
2024-10-21 10:26:46 +0900

    ログイン・ログアウト機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,         presence: true
-  validates :last_name_kanji,  presence: true
-  validates :first_name_kanji, presence: true
-  validates :last_name_kana,   presence: true
-  validates :first_name_kana,  presence: true
+  validates :last_name_kanji,  presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'を正確に入力してください' }
+  validates :first_name_kanji, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'を正確に入力してください' }
+  validates :last_name_kana,   presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'を正確に入力してください' }
+  validates :first_name_kana,  presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'を正確に入力してください' }
   validates :birthday,         presence: true
 
   has_many :items
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
    root to:"items#index"
    
+   resources :items, only: [:new, :create, :show, :edit, :update, :destroy]
+   resources :users, only: [:new, :create]
 end
