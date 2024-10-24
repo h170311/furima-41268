47799cd254cd385d6d1e3cdef1d7cc365c9d4e96
pmrfx387 <h170311@tg.commufa.jp>
2024-10-24 14:49:59 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,17 @@ class Item < ApplicationRecord
   belongs_to :item_status
   belongs_to :prefecture
   belongs_to :shipping_cost
-  
+
+  validates :image,            presence: true
+  validates :item_name,        presence: true
+  validates :item_description, presence: true
+  validates :category_id,      numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :item_status_id,   numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :shipping_cost_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :prefecture_id,    numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :delivery_time_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :price, presence: true, format: { with: /\A[300-9999999\d]+\z/, message: 'を正確に入力してください' }
+
   # has_one    :buy
   belongs_to :user
   has_one_attached :image
