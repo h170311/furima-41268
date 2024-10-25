c3ac376defbe3cf63e9556753c57318c81ea0844
pmrfx387 <h170311@tg.commufa.jp>
2024-10-25 17:14:31 +0900

    出品した商品の情報をテーブルに保存

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,11 +9,11 @@ class Item < ApplicationRecord
   validates :image,            presence: true
   validates :item_name,        presence: true
   validates :item_description, presence: true
-  validates :category_id,      numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :item_status_id,   numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :shipping_cost_id, numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :prefecture_id,    numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :delivery_time_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :category_id,      numericality: { other_than: 1, message: "can't be blank" }
+  validates :item_status_id,   numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_cost_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id,    numericality: { other_than: 0, message: "can't be blank" }
+  validates :delivery_time_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :price, presence: true, format: { with: /\A[300-9999999\d]+\z/, message: 'を正確に入力してください' }
 
   # has_one    :buy
--- app/models/item_status.rb
+++ app/models/item_status.rb
@@ -1,4 +1,4 @@
-class Item_status < ActiveHash::Base
+class ItemStatus < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
     { id: 2, name: '新品・未使用' },
@@ -7,10 +7,9 @@ class Item_status < ActiveHash::Base
     { id: 5, name: 'やや傷や汚れあり' },
     { id: 6, name: '傷や汚れあり' },
     { id: 7, name: '全体的に状態が悪い' }
-    
+
   ]
 
   include ActiveHash::Associations
   has_many :items
-  
- end
+end
