e080b519eb2b48f0ca52cc490ad0c181207652f9
pmrfx387 <h170311@tg.commufa.jp>
2024-10-30 15:30:27 +0900

    テストコード実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name { 'いす' }
+    item_description { '柔らかくて大きい' }
+    category_id { 2 }
+    item_status_id { 2 }
+    shipping_cost_id          { 2 }
+    prefecture_id             { 1 }
+    delivery_time_id          { 2 }
+    price { 2000 }
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,73 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    user = FactoryBot.create(:user)
+    @item = FactoryBot.build(:item, user_id: user.id)
+  end
+  describe '出品情報の保存' do
+    context '内容に問題ない場合' do
+      it 'すべての情報があれば登録できる' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '内容に問題がある場合' do
+      it 'item_nameが空では登録できない' do
+        @item.item_name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Item name can't be blank"
+      end
+      it 'item_descriptionが空では登録できない' do
+        @item.item_description = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Item description can't be blank"
+      end
+      it 'categoryを選択していないと保存できないこと' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'item_statusを選択していないと保存できないこと' do
+        @item.item_status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item status can't be blank")
+      end
+      it 'shipping_costを選択していないと保存できないこと' do
+        @item.shipping_cost_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping cost can't be blank")
+      end
+      it 'prefectureを選択していないと保存できないこと' do
+        @item.prefecture_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it 'delivery_timeを選択していないと保存できないこと' do
+        @item.delivery_time_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery time can't be blank")
+      end
+      it 'priceが空だと保存できないこと' do
+        @item.price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it 'priceが全角数字だと保存できないこと' do
+        @item.price = '２０００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price を正確に入力してください')
+      end
+      it 'priceが300円未満では保存できないこと' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price を正確に入力してください')
+      end
+      it 'priceが10,000,000円を超過すると保存できないこと' do
+        @item.price = 10_000_000
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price を正確に入力してください')
+      end
+    end
+  end
 end
