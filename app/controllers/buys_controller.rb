6964038ab7daf1777f186fd8e4e7d3199a2e8ef7
pmrfx387 <h170311@tg.commufa.jp>
2024-11-12 11:45:29 +0900

    ルーティング及びコントローラーに記述

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -2,6 +2,22 @@ class BuysController < ApplicationController
   def index
   end
 
+  def new
+    @buy = OrderForm.new
+  end
+
   def create
+    @buy = OrderForm.new(set_params)
+    return unless @buy.valid?
+
+    @buy.save
+    redirect_to root_path
+  end
+
+  private
+
+  def set_params
+    params.permit(:item_id, :number, :expiry, :cvc, :postcode, :prefecture_id, :municipalities, :street_address,
+                  :telephone, :building_name).merge(user_id: current_user.id)
   end
 end
