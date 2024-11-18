11fff738e4f37005c2130121f98906b2309298e0
pmrfx387 <h170311@tg.commufa.jp>
2024-11-18 17:52:47 +0900

    テストコードを実装

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,12 +5,13 @@ class Order
 
   validates :user_id, presence: true
   validates :item_id, presence: true
-  validates :postcode,        presence: true, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+  validates :postcode,        presence: true,
+                              format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
   validates :prefecture_id,   numericality: { other_than: 0, message: "can't be blank" }
   validates :municipalities,  presence: true
   validates :street_address,  presence: true
-  validates :telephone,       presence: true
-
+  validates :telephone,       presence: true,
+                              format: { with: /\A[0-9]{10,11}+\z/, message: 'is invalid. Input half-width characters.' }
   def save
     buy = Buy.create(user_id:, item_id:)
     BuyerAddress.create(buy_id: buy.id, postcode:, prefecture_id:, municipalities:, street_address:, telephone:)
---
+++ spec/factories/orders.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :order do
+    postcode { '456-1234' }
+    prefecture_id { 1 }
+    municipalities { '名古屋市' }
+    street_address { '青山1-1-1' }
+    building_name { 'いいビル777' }
+    telephone { 1_234_567_890 }
+    item_id { 5 }
+  end
+end
