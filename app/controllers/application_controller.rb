92cc5af02d890beff445b412891730823dd2132b
pmrfx387 <h170311@tg.commufa.jp>
2024-11-25 15:50:13 +0900

    Basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   # before_action :basic_auth
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?, :basic_auth
 
   private
 
@@ -8,6 +8,12 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up,
                                       keys: [:nickname, :last_name_kanji, :first_name_kanji, :last_name_kana, :first_name_kana,
                                              :birthday])
+  end
+  
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '2222'
+      end
   end
 
   # def basic_auth
